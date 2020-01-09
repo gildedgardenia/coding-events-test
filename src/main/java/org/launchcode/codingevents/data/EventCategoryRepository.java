a6d4864e08e6864b2f8b083903fce157b01d242b
Chris Bay <chris@chrisbay.net>
2020-01-09 13:44:18 -0600

    Lesson 16 exercises solution

---
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.EventCategory;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Chris Bay
+ */
+public interface EventCategoryRepository extends CrudRepository<EventCategory, Integer> {
+
+
+
+}
